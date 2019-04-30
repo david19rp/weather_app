53c255da7e5b0d921cf95809a7544fece3dec012
Your Name <user@MacBook-Pro-de-Daniela.local>
2019-04-29 20:23:19 -0600

    adding the 3 files of the projects

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+let $endGame="https://api.openweathermap.org/data/2.5/weather?q=Heredia,CR&appid=a7885bf35c64037d8462ae5f2ecbb0ac";
+
+
+
+// d6ac9a8b8d7c463ad353c08b092e0cd9
+
+
+
+
+
+// api.openweathermap.org/data/2.5/weather?q=Heredia,CR&appid=a7885bf35c64037d8462ae5f2ecbb0ac
