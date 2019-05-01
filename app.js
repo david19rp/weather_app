ada84d52bdd38c64fccb468dde6ab438bb143691
Your Name <user@MacBook-Pro-de-Daniela.local>
2019-05-01 03:03:38 -0600

    adding functionality

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 
+$(document).ready(function(){
+    let $myPoint = "https://api.openweathermap.org/data/2.5/weather?q=";
+    $("#submit").on('click',function(){
+        let $inputValue = $("#inputVal").val();
+        
+        if($inputValue!= ""){
+            console.log($inputValue);
+        }else{
+            console.log("salio");
+        }
+    });
+});
 
-let $endGame="https://api.openweathermap.org/data/2.5/weather?q=Heredia,CR&appid=a7885bf35c64037d8462ae5f2ecbb0ac";
 
 
+  
+
+    // $.get($myPoint, function (data,status) {
+        
+    // })
 
 // d6ac9a8b8d7c463ad353c08b092e0cd9
 
 
 
+//Heredia,CR&appid=a7885bf35c64037d8462ae5f2ecbb0ac
 
-
-// api.openweathermap.org/data/2.5/weather?q=Heredia,CR&appid=a7885bf35c64037d8462ae5f2ecbb0ac
