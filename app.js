44b422c8819b42118b16a4ca6fbff7ed289c7e10
Your Name <user@MacBook-Pro-de-Daniela.local>
2019-05-03 02:54:40 -0600

    functionality is ready and working fine in staging

--- app.js
+++ app.js
@@ -4,20 +4,27 @@ $(document).ready(function(){
     $("#submit").on('click',function(){
         
         let $inputValue = $("#inputVal").val();
-        let $myKey = "&APPID=d6ac9a8b8d7c463ad353c08b092e0cd9";
+        let $myKey = "&units=metric&APPID=d6ac9a8b8d7c463ad353c08b092e0cd9";
         let $myPoint = "https://api.openweathermap.org/data/2.5/weather?q="+$inputValue+$myKey;
-        console.log($myPoint);
-        if($inputValue!=""){
+        if($inputValue!= ''){
             $.get($myPoint, function (data,status) {
-                console.log("the temp is" +" "+ data);
-                // let total = data.weather[0];    
-                // return "<p>total</p>";    
+                let widget = show(data);
+                $("#show").html(widget);
+                // $("#inputVal").val('');
             })
         }else{
-            console.log("salio");
+            $("#show").html("<h1>Put the name of the region, this is a error!! Alert! Alert!</h1>");
         }
     });
 });
+
+function show(data){
+    return "<h2>"+data.name+"</h2>" +
+    "<h3><strong>Weather</strong> : "+ data.weather[0].main+"</h3>" +
+    "<h3><strong>Weather</strong> : "+ data.weather[0].description+"</h3>" +
+    "<h3><strong>Temp</strong> : "+ data.main.temp+"</h3>" +
+    "<h3><strong>Humidity</strong> : "+ data.main.humidity+"</h3>" 
+}
 
 
 
